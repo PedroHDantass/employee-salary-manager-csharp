9841dae3b0423b9ccc22450458ec87dfc2902245
PedroHDantas <pedrohdantaspc@gmail.com>
2025-07-04 11:02:03 -0300

    feat: adc cadastro de funcionario

--- Program.cs
+++ Program.cs
@@ -5,7 +5,27 @@ namespace ListEX
     {
         static void Main(string[] args)
         {
-          
+
+            List<Employee> employees = new List<Employee>();
+
+            Console.Write("How many employees will be registered? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Employee #" + i);
+
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine());
+
+                employees.Add(new Employee(id, name, salary));
+            }
         }
     }
 }
