591c14091494826ed4c55b6b2981ecae311c1dee
PedroHDantas <pedrohdantaspc@gmail.com>
2025-07-04 10:41:27 -0300

    feat: adc construtor e método para aumento de salário

--- Employee.cs
+++ Employee.cs
@@ -7,5 +7,17 @@
         public string Name { get; set; }
 
         public double Salary { get; private set; }
+
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(double percentage)
+        {
+            Salary += Salary * (Salary + (percentage / 100));
+        }
     }
 }
